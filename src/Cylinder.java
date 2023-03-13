0dfec7bb7f7d35ab94335c9f48fe892c530647a3
HaiYen42 <haiyentth42@gmail.com>
2023-03-13 15:47:17 +0700

    Th3

---
+++ src/Cylinder.java
@@ -0,0 +1,24 @@
+public class Cylinder {
+    public static double getVolume(int radius, int height){
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        double volume = getVolume(height, baseArea, perimeter);
+        return volume;
+    }
+
+    public static double getVolume(int height, double baseArea, double perimeter) {
+        double volume = perimeter * height + 2* baseArea;
+        return volume;
+    }
+
+    public static double getPerimeter(int radius) {
+        double perimeter = 2* Math.PI* radius;
+        return perimeter;
+    }
+
+    public static double getBaseArea(int radius) {
+        double baseArea = Math.PI * radius * radius;
+        return baseArea;
+    }
+
+}
